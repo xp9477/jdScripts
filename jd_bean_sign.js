3823e37fca37e2530091d5a62ed00f032f222f17
徐鹏 <947760133@qq.com>
2021-07-18 21:51:50 +0800

    Update jd_bean_sign.js

--- jd_bean_sign.js
+++ jd_bean_sign.js
@@ -6,7 +6,7 @@ IOS软件用户请使用 https://raw.githubusercontent.com/NobyDa/Script/master/
 更新时间：2021-5-6
 推送通知默认简洁模式(多账号只发送一次)。如需详细通知，设置环境变量 JD_BEAN_SIGN_NOTIFY_SIMPLE 为false即可(N账号推送N次通知)。
 Modified From github https://github.com/ruicky/jd_sign_bot
-0 7 * * * jd_bean_sign.js
+1 0 * * * jd_bean_sign.js
 */
 const $ = new Env('京东多合一签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
