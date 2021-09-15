06866794016de40fbe680f7bb0adebecdde5ea40
大鹏 <947760133@qq.com>
2021-09-16 01:27:51 +0800

    Update jd_health.js

--- jd_health.js
+++ jd_health.js
@@ -1,10 +1,13 @@
 /*
-
 东东健康社区
-更新时间：2021-8-9
+更新时间：2021-4-22
 活动入口：京东APP首页搜索 "玩一玩"即可
 
-13 1,8,21 * * * jd_health.js
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+===================quantumultx================
+[task_local]
+#东东健康社区
+13 1,6,22 * * * jd_health.js
  */
 const $ = new Env("东东健康社区");
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
@@ -124,6 +127,28 @@ function getTaskDetail(taskId = '') {
               if (data?.data?.result?.taskVos) {
                 console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${data?.data?.result?.taskVos[0].assistTaskDetailVo.taskToken}\n`);
                 // console.log('好友助力码：' + data?.data?.result?.taskVos[0].assistTaskDetailVo.taskToken)
+
+                // ***************************
+                // 报告运行次数
+                if(data?.data?.result?.taskVos[0].assistTaskDetailVo.taskToken){
+                  $.get({
+                  url: `https://cdn.nz.lu/api/runTimes?activityId=health&sharecode=${data?.data?.result?.taskVos[0].assistTaskDetailVo.taskToken}`,
+                  headers: {
+                    'Host': 'api.jdsharecode.xyz'
+                  },
+                  timeout: 10000
+                  }, (err, resp, data) => {
+                    if (err) {
+                      console.log('上报失败', err)
+                    } else {
+                      if (data === '1' || data === '0') {
+                        console.log('上报成功')
+                      }
+                    }
+                  })
+                }
+                // ***************************
+
               }
             } else if (taskId === 22) {
               console.log(`${data?.data?.result?.taskVos[0]?.taskName}任务，完成次数：${data?.data?.result?.taskVos[0]?.times}/${data?.data?.result?.taskVos[0]?.maxTimes}`)
@@ -132,7 +157,7 @@ function getTaskDetail(taskId = '') {
               await $.wait(1000 * (data?.data?.result?.taskVos[0]?.waitDuration || 3));
               await doTask(data?.data?.result?.taskVos[0].shoppingActivityVos[0]?.taskToken, 22, 0);//完成任务
             } else {
-              for (let vo of data?.data?.result?.taskVos.filter(vo => vo.taskType !== 19) ?? []) {
+              for (let vo of data?.data?.result?.taskVos.filter(vo => vo.taskType !== 19 && vo.taskType !== 25) ?? []) {
                 console.log(`${vo.taskName}任务，完成次数：${vo.times}/${vo.maxTimes}`)
                 for (let i = vo.times; i < vo.maxTimes; i++) {
                   console.log(`去完成${vo.taskName}任务`)
@@ -215,6 +240,8 @@ function exchange(commodityType, commodityId) {
             if ($.isNode()) {
               allMessage += `【京东账号${$.index}】 ${$.UserName}\n兑换${data.data.result.jingBeanNum}京豆成功🎉${$.index !== cookiesArr.length ? '\n\n' : ''}`
             }
+          } else {
+            console.log(data.data.bizMsg)
           }
         }
       } catch (e) {
@@ -310,8 +337,11 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://share.turinglabs.net/api/v3/health/query/${randomCount}/`,
-      'timeout': 10000
+      url: `https://cdn.nz.lu/api/health/${randomCount}`,
+      headers: {
+        'Host': 'api.jdsharecode.xyz'
+      },
+      timeout: 1
     }, (err, resp, data) => {
       try {
         if (err) {
@@ -345,10 +375,10 @@ function shareCodesFormat() {
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
-    // const readShareCodeRes = await readShareCode();
-    // if (readShareCodeRes && readShareCodeRes.code === 200) {
-    //   $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
-    // }
+    const readShareCodeRes = await readShareCode();
+    if (readShareCodeRes && readShareCodeRes.code === 200) {
+      $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
+    }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
