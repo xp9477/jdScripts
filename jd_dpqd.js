bd0b0026be2b17e2a7627798b750e677ea8a82ee
大鹏 <947760133@qq.com>
2022-02-15 18:48:01 +0800

    。

--- jd_dpqd.js
+++ jd_dpqd.js
@@ -1,23 +1,17 @@
 /*
-cron 0 0 * * * jd_dpqd.js
 店铺签到，各类店铺签到，有新的店铺直接添加token即可
-搬运cui521大佬脚本，请勿外传！！！
-环境变量:
-DPQDTK: token1&token2
-仓库不再提供token
+============Quantumultx===============
+[task_local]
+#店铺签到
+15 2,14 * * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js, tag=店铺签到, enabled=true
+===========Loon============
+[Script]
+cron "15 2,14 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js,tag=店铺签到
+============Surge=============
+店铺签到 = type=cron,cronexp="15 2,14 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.js
+===========小火箭========
+店铺签到 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_shop_sign.jss, cronexpr="15 2,14 * * *", timeout=3600, enable=true
 */
-let token = []
-if (process.env.DPQDTK) {
-  if (process.env.DPQDTK.includes('\n')) {
-    token = [...process.env.DPQDTK.split('\n'),...token]
-  } else {
-    token = [...process.env.DPQDTK.split('&'),...token]
-  }
-}
-if (!token.length) {
-  console.log('无店铺签到token,不执行.需自备token:环境变DPQDTK: tk1&tk2.')
-  return
-}
 const $ = new Env('店铺签到');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
@@ -30,6 +24,26 @@ let activityId=''
 let vender=''
 let num=0
 let shopname=''
+const token = [
+  "B8167EC9BAC16BBD9284DD3C757D60D3",
+  "4C17B0D0B81E4874662357E7F558D2FB",
+  "A518A5F74992F82258E9E859443A7CFA",
+  "D8FBBB03E5B3F7671BDCB73728957C56",
+  "F658709F1069375C7F7EB1D2DB8A85DD",
+  "E3B62366D81FA9DC53E5E1088FD5FA46",
+  "9C6F70D339EA675C0CC0A2D7FBECCC57",
+  "247751BD7E2B94A0C91E1FA956841022",
+  "69B31DB107B9FE242E286E4E8B3E0E75",
+  "1071B720BDB54B6ED8163A6B4116F35F",
+  "448830F150804FF9EF123B2BABBD00EA",
+  "EC519D173D371D156FCB3EB3D07D0CF2",
+  "3F1ACF8E669258AFABAE6D5084AD4138",
+  "8049AC5EE59DBBB7146F6BA61E494CC2",
+  "CDE449E3CCFC77AA637133C88399EAC5",
+  "186ACAEA426F9C07CB2E0BC790DA78FA",
+  "E5254610D0CF9752133E206292D83256",
+  "E314BA43DB1C59C7A58AB5BAB929D604"
+]
 
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
--- jd_xinruimz.js
+++ jd_xinruimz.js
@@ -207,7 +207,7 @@ async function fertilizerdotask() {
                 $.log("任务完成")
             }
         }
-        if ($.fertilizertasklist.prodcuts && ["card","car"].includes(process.env.FS_LEVEL)) {
+        if ($.fertilizertasklist.prodcuts) {
             $.log("去完成加购任务..")
             if ($.fertilizertasklist.prodcuts.length != $.fertilizerlist.view_product.length) {
                 for (const vo of $.fertilizertasklist.prodcuts) {
